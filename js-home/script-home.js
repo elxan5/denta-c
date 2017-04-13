5c79e16699ace0250f172f5573aa96fed99e645d
Kamran Softrock <amorfrap@gmail.com>
2017-04-14 00:56:40 +0400

    home 90% completed

---
+++ js-home/script-home.js
@@ -0,0 +1,58 @@
+$(function() {
+    $('.carousel').each(function(){
+        $(this).carousel({
+            interval: false
+        });
+    });
+})
+
+$(document).ready(function(){
+  $(".fquote").click(
+    function(){
+      $(this).css({"background-color":"#18AFD3"});
+      $(".squote, .tquote").css({"background-color":"lightgray"});
+      $(".bir").show();
+      $(".iki, uch").hide();
+  })
+
+  $(".squote").click(
+    function(){
+      $(this).css({"background-color":"#18AFD3"});
+      $(".fquote, .tquote").css({"background-color":"lightgray"});
+      $(".iki").show();
+      $(".bir, .uch").hide();
+  })
+
+  $(".tquote").click(
+    function(){
+      $(this).css({"background-color":"#18AFD3"});
+      $(".fquote, .squote").css({"background-color":"lightgray"});
+      $(".uch").show();
+      $(".bir, .iki").hide();
+  })
+})
+
+$(".t1").click(function(){
+  $(this).css({"background-color":"#18AFD3"});
+  $(".t2, .t3").css({"background-color":"lightgray"});
+  $(".team1").show();
+  $(".team3").css({"right":"-500%"});
+  $(".team2").css({"right":"-200%"})
+})
+
+$(".t2").click(function(){
+  $(this).css({"background-color":"#18AFD3"});
+  $(".t1, .t3").css({"background-color":"lightgray"});
+  $(".team2").css({
+    "right":"200%"
+  });
+  $(".team3").css({"right":"-300%"})
+})
+
+$(".t3").click(function(){
+  $(this).css({"background-color":"#18AFD3"});
+  $(".t1, .t2").css({"background-color":"lightgray"});
+  $(".team3").css({
+    "right":"300%"
+  });
+})
---
+++ js-services/script.js
@@ -0,0 +1,63 @@
+window.onscroll = function(){
+	if(window.pageYOffset > 105){
+		document.querySelector(".aqua").style.position = "fixed";
+		document.querySelector(".aqua").style.top = "0px";
+		document.querySelector(".aqua").style.width = "100%";
+		document.querySelector(".aqua").style.borderRadius = "0px";
+
+	}else{
+		document.querySelector(".aqua").style.position = "";
+		document.querySelector(".aqua").style.top = "";
+		document.querySelector(".aqua").style.width = "";
+		document.querySelector(".aqua").style.borderRadius = "";
+	}
+}
+
+$(".leftPart ul li:first-child").click(function(){
+	$(".hiding").hide();
+	$(".general").show();
+})
+$(".leftPart ul li:nth-child(2)").click(function(){
+	$(".hiding").hide();
+	$(".cosmetic").show();
+})
+$(".leftPart ul li:nth-child(3)").click(function(){
+	$(".hiding").hide();
+	$(".dental").show();
+})
+$(".leftPart ul li:nth-child(4)").click(function(){
+	$(".hiding").hide();
+	$(".teeth").show();
+})
+$(".leftPart ul li:nth-child(5)").click(function(){
+	$(".hiding").hide();
+	$(".crowns").show();
+})
+$(".leftPart ul li:last-child").click(function(){
+	$(".hiding").hide();
+	$(".dentures").show();
+})
+
+$(".headerInput input").click(function(){
+	$(".aqua h5").css({
+		"font-size": "10px",
+		"line-height": "10px"
+	});
+});
+
+$(".aqua ul span i").click(function(){
+	$(".aqua h5").css({
+		"font-size": "",
+		"line-height": ""
+	});
+	$(this).hide();
+	$(".aqua h5 i").show();
+	$(".aqua ul li").css({"display":"none"});
+	$(".aqua .headerInput").show("slow");
+});
+$(".aqua h5 i").click(function(){
+	$(this).hide();
+	$(".aqua ul span i").show();
+	$(".aqua ul li").show("slow");
+	$(".aqua .headerInput").css({"display":"none"});
+});
